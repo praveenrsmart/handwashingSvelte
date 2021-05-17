c7cbfd7580e027ae31f17c70fbd19bae97e588dd
praveenrsmart <praveenrsmart@gmail.com>
2021-05-17 12:36:36 +0530

    got rollup config working

--- rollup.config.js
+++ rollup.config.js
@@ -43,6 +43,7 @@ export default {
 				dev: !production
 			}
 		}),
+		css({output:'public/build/extra.css'}),
 		// we'll extract any component CSS out into
 		// a separate file - better for performance
 		css({ output: 'bundle.css' }),
