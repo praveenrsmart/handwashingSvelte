560d287273d723e7842087d909e040b8300cc1e1
praveenrsmart <praveenrsmart@gmail.com>
2021-05-18 18:52:56 +0530

    commiting HTML

--- src/App.svelte
+++ src/App.svelte
@@ -1,1 +1,12 @@
-<h1>Test</h1>
+<script>
+	import Timer from "./Timer.svelte";
+	import HowTo from "./HowTo.svelte";
+</script>
+<style>
+	h1{
+		text-align: center;
+	}
+</style>
+<h1>Handwashing App</h1>
+<Timer />
+<HowTo />
